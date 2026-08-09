425531330fa55fdf04e46cf0eec988872cd42e27
parth gupta <parthgupta3421@gmail.com>
2026-08-09 15:49:38 +0000

    2nd problem compelete

--- psetsCS50/pset2.c
+++ psetsCS50/pset2.c
@@ -56,6 +56,33 @@
 //     }
 // }
 
-// lets start the second problem of the pset2 
+// lets start the second problem of the pset2
 
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        return 1;
+    }
+    int key = atoi(argv[1]);
+    string text = get_string("Enter your text: ");
+
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        if (isupper(text[i]))
+        {
+            text[i]=((text[i]-'A'+key)%26)+'A';
+        }
+        else if (islower(text[i]))
+        {
+            text[i]=((text[i]-'a'+key)%26)+'a';
+        }
+    }
+    printf("ciphertext: %s\n",text);
+}
