6490cac5e33eae4c0f201ab9d9eada5de3747fbb
parth gupta <parthgupta3421@gmail.com>
2026-08-10 12:48:34 +0000

    full pset2 done

--- psetsCS50/pset2.c
+++ psetsCS50/pset2.c
@@ -87,7 +87,61 @@
 //     printf("ciphertext: %s\n",text);
 // }
 
+// lets start the third and the last problem of the pset2
 
-// lets start the third and the last problem of the pset2 
+// this is the subsitution problem of the problem set 2
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        return 1;
+    }
+    
+    if (strlen(argv[1])!=26)
+    {
+        return 1;
+    }
+    
+    for (int i = 0; i<26;i++)
+    {
+        if (isalpha(argv[1][i])==0)
+        {
+            return 1;
+        }
+        for (int j=i+1; j<26;j++)
+        {
+            if(tolower(argv[1][i]) == tolower(argv[1][j]))
+            {
+                return 1;
+            }
+            
+        }
+    }
+
+    string input = get_string("Enter your text: ");
+
+    for (int i = 0; input[i] != '\0'; i++)
+
+    {
+        if (isupper(input[i]))
+        {
+            int position = input[i]-'A';
+            input[i]= argv[1][position];
+        }
+        else if (islower(input[i]))
+        {
+            int position = input[i]-'a';
+            tolower(input[i]= argv[1][position]);
+        }
+    }
 
 
+    printf("ciphertext: %s\n", input);
+}
